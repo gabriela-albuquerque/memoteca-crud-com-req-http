bda116254af0d6148bf746d938a6ebe801f6b5a1
Gabriela Albuquerque <126294239+gabriela-albuquerque@users.noreply.github.com>
2024-09-23 17:38:09 -0300

    criada a funcao que envia novos pensamentos ao banco de dados

--- js/ui.js
+++ js/ui.js
@@ -2,7 +2,7 @@ import api from "./api.js"
 
 const ui = {
     async renderizarPensamentos() {
-        const listaPensamentos = document.getElementById(lista - pensamentos)
+        const listaPensamentos = document.getElementById("lista-pensamentos");
 
         try {
             const pensamentos = await api.buscarPensamentos()
