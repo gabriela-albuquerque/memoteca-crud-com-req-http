35a90e771b201d22d5d8d0b657b97bb85a897845
Gabriela Albuquerque <126294239+gabriela-albuquerque@users.noreply.github.com>
2024-10-12 15:39:10 -0300

    ajustes no regexConteudo para incluir acentuacao e pontuacao

--- js/main.js
+++ js/main.js
@@ -1,11 +1,26 @@
 import ui from "./ui.js";
 import api from "./api.js";
 
+const pensamentosSet = new Set();
+
+async function adicionarChaveAoPensamento() {
+    try {
+        const pensamentos = await api.buscarPensamentos();
+        pensamentos.forEach(pensamento => {
+            const chavePensamento = 
+            `${pensamento.conteudo.trim().toLowerCase()}-${pensamento.autoria.trim().toLowerCase()}`;
+            pensamentosSet.add(chavePensamento);
+        })
+    } catch (error) {
+        alert('Erro ao adicionar chave ao pensamento');
+    }
+}
+
 function removerEspacos(string) {
     return string.replaceAll(/\s+/g, '');
 }
 
-const regexConteudo = /^[A-Za-z\s]{10,}$/;
+const regexConteudo = /^[A-Za-zÀ-ÖØ-öø-ÿ\s.,!?;:()\-]{10,}$/;
 const regexAutoria = /^[A-Za-z]{3,15}$/;
 
 function validarConteudo(conteudo) {
@@ -17,6 +32,7 @@ function validarAutoria(autoria) {
 
 document.addEventListener("DOMContentLoaded", () => {
     ui.renderizarPensamentos();
+    adicionarChaveAoPensamento()
 
     const formularioPensamento = document.getElementById("pensamento-form");
     const botaoCancelar = document.getElementById("botao-cancelar");
@@ -49,6 +65,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
         if (!validarData(data)) {
             alert("Não é permitido o cadastro de datas futuras. Selecione outra data.")
+            return
+        }
+
+        const chaveNovoPensamento = 
+        `${conteudo.trim().toLowerCase()}-${autoria.trim().toLowerCase()}`;
+
+        if(pensamentosSet.has(chaveNovoPensamento)) {
+            alert('Esse pensamento já existe');
             return
         }
 
